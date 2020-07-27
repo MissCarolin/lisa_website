ea57e7bbc14eb278dfd682aa2a76bf796ebf8bee
Carolin Wagner <carolin.wagner.hh@gmx.de>
2020-07-27 19:05:52 +0200

    implemented slider for images

--- app/javascript/components/image_slider.js
+++ app/javascript/components/image_slider.js
@@ -1,10 +1,11 @@
 
-
 const slider = () => {
+
 const name = document.querySelector('.name_index h1')
 const description1 = document.querySelector('.description_index h2')
 const description2 = document.querySelector('.description_index h3')
-const pictures = document.querySelectorAll('.images')
+const pictures = [...document.querySelectorAll('.images')]
+
 
 window.addEventListener('load', (event) => {
   console.log(event)
@@ -15,13 +16,21 @@ window.addEventListener('load', (event) => {
     description2.classList.add('active')
     ;
   }, 300);
-
-
-  // pictures.forEach(picture => {
-  // picture.classList.add('active')
-  // })
 })
 
+  window.addEventListener('scroll', (event) => {
+     pictures.map(picture => {
+      const slideInAt = (window.scrollY + window.innerHeight) - picture.height / 2;
+      const imageBottom = picture.offsetTop + picture.height
+      const isHalfShown = slideInAt > picture.offsetTop;
+      const isNotScrolledPast = window.scrollY < imageBottom;
+      if (isHalfShown && isNotScrolledPast) {
+        picture.classList.add('active');
+      } else {
+        picture.classList.remove('active')
+      }
+    })
+  });
 
 
 }
