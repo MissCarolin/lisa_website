f69475a6b35dd4b653b93839e32b8ece27b6ec7a
Carolin Wagner <carolin.wagner.hh@gmx.de>
2020-07-27 15:32:47 +0200

    added new js slider

--- app/javascript/components/image_slider.js
+++ app/javascript/components/image_slider.js
@@ -2,12 +2,19 @@
 
 const slider = () => {
 const name = document.querySelector('.name_index h1')
-const description = document.querySelector('.description_index h2')
+const description1 = document.querySelector('.description_index h2')
+const description2 = document.querySelector('.description_index h3')
 const pictures = document.querySelectorAll('.images')
 
 window.addEventListener('load', (event) => {
   console.log(event)
   name.classList.add('active')
+
+  setTimeout(() => {
+    description1.classList.add('active')
+    description2.classList.add('active')
+    ;
+  }, 350);
 
 
   // pictures.forEach(picture => {
