ec2dd817812c8a4cf1b74d77403a68cbed1436f3
Carolin Wagner <carolin.wagner.hh@gmx.de>
2020-09-30 14:23:44 +0200

    added styling to header

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
  root to: 'pictures#index'
-get '/impressum', to: 'pictures#impressum', as: :impressum
+ get '/impressum', to: 'pictures#impressum', as: :impressum
   resources :pictures, only: [:index, :show, :new, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
