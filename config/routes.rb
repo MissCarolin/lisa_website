abb92c97990b1027526e4e471b468c2f5f07de66
Carolin Wagner <carolin.wagner.hh@gmx.de>
2020-10-01 14:22:23 +0200

    added home page + pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
- root to: 'pictures#index'
+ root to: 'pages#home'
  get '/impressum', to: 'pictures#impressum', as: :impressum
   resources :pictures, only: [:index, :show, :new, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
