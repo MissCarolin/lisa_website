8577612fbffa6d7885227eb54d78238ea62bfea1
MissCarolin <carolin.wagner.hh@gmx.de>
2021-02-21 21:53:08 +0100

    optimized home page for mobile

---
+++ app/javascript/plugins/init_sortable.js
@@ -0,0 +1,11 @@
+// import Sortable from 'sortablejs';
+
+// const initSortable = () => {
+//     const list = document.querySelector(".sortable-pictures");
+//     Sortable.create(list);
+// };
+
+// initSortable();
+
+
+// export { initSortable };
