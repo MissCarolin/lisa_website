307f66704c30269b6ef2d250f1ea98fb1f746a52
MissCarolin <carolin.wagner.hh@gmx.de>
2023-04-04 23:25:30 +0200

    deleted sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable neu
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
 end
